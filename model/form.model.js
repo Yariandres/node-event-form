39ba0a588530d6038f2b890cffa94a6684cc529c
Yari Andres <yari.andres@gmail.com>
2021-02-18 20:50:28 +0100

    defining the event model & installing mongoose

---
+++ model/form.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+	firstName: {
+		type: 'string',
+		required: true,
+	},
+	lastName: {
+		type: 'string',
+		required: true,
+	},
+	email: {
+		type: 'string',
+		required: true,
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
+});
+
+const EventModel = mongoose.model("Event", EventSchema);
+
+module.exports = EventModel;
