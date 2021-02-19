c0684c345bcc6b0d88890ea362cd78144972e6d7
Yari Andres <yari.andres@gmail.com>
2021-02-19 23:00:10 +0100

    passing all the tests and returning json error message instead of htmt

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express = require('express'); 
-const eventRoutes = require("./routes/event.routes");
+const express = require('express');
+const eventRoutes = require('./routes/event.routes');
 const app = express();
-const mongodb = require("./momgodb/mongodb.connect");
+const mongodb = require('./momgodb/mongodb.connect');
 
 mongodb.connect();
 
 app.use(express.json());
 
-app.use("/events", eventRoutes);
+app.use('/events', eventRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello world!")
+app.use((error, req, res, next) => {
+	res.status(500).json({ message: error.message });
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello world!');
 });
 
 module.exports = app;
-
-
--- tests/intergration/event.controller.int.test.js
+++ tests/intergration/event.controller.int.test.js
@@ -6,13 +6,25 @@ const endpointUrl = '/events/';
 
 describe(endpointUrl, () => {
 	it('POST' + endpointUrl, async () => {
-		const response = await request(app)
-            .post(endpointUrl)
-            .send(newEvent);
-            expect(response.statusCode).toBe(201);
-            expect(response.body.firstName).toBe(newEvent.firstName);
-            expect(response.body.lastName).toBe(newEvent.lastName);
-            expect(response.body.email).toBe(newEvent.email);
-            expect(response.body.date).toBe(newEvent.date);
+		const response = await request(app).post(endpointUrl).send(newEvent);
+		expect(response.statusCode).toBe(201);
+		expect(response.body.firstName).toBe(newEvent.firstName);
+		expect(response.body.lastName).toBe(newEvent.lastName);
+		expect(response.body.email).toBe(newEvent.email);
+		expect(response.body.date).toBe(newEvent.date);
 	});
+
+	it(
+		'Should return error 500 on malformed data with the POST' + endpointUrl,
+		async () => {
+			const response = await request(app)
+				.post(endpointUrl)
+				.send({ title: 'Missing date property' });
+			expect(response.statusCode).toBe(500);
+			expect(response.body).toStrictEqual({
+				message:
+                        "Event validation failed: date: Path `date` is required., email: Path `email` is required., lastName: Path `lastName` is required., firstName: Path `firstName` is required."
+			});
+		},
+	);
 });
