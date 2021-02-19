9708cbaf8d2f2a053753d526a52c9d215857c242
Yari Andres <yari.andres@gmail.com>
2021-02-19 22:35:56 +0100

    passing the test by wrapping  createEvent func in a try catch

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -1,7 +1,10 @@
-
-const EventModel = require("../model/event.model");
+const EventModel = require('../model/event.model');
 
 exports.createEvent = async (req, res, next) => {
-    const createdModel = await EventModel.create(req.body);
-    res.status(201).json(createdModel);
+	try {
+		const createdModel = await EventModel.create(req.body);
+		res.status(201).json(createdModel);
+	} catch (err) {
+        next(err);
+    }
 };
