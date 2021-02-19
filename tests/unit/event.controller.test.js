e83de024e9aa88d754e39826eaac6c4c8a2f90a0
Yari Andres <yari.andres@gmail.com>
2021-02-19 22:33:31 +0100

    mocked error handling failed test

--- tests/unit/event.controller.test.js
+++ tests/unit/event.controller.test.js
@@ -1,43 +1,52 @@
-const EventController = require("../../controllers/event.controller");
-const EventModel = require("../../model/event.model");
-const httpMocks = require("node-mocks-http");
-const newEvent = require("../mock-data/new-event.json");
+const EventController = require('../../controllers/event.controller');
+const EventModel = require('../../model/event.model');
+const httpMocks = require('node-mocks-http');
+const newEvent = require('../mock-data/new-event.json');
 
 EventModel.create = jest.fn();
 
 let req, res, next;
 
 beforeEach(() => {
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
+	req = httpMocks.createRequest();
+	res = httpMocks.createResponse();
+	next = jest.fn();
 });
 
-describe("EventController.createEvent", () =>{
+describe('EventController.createEvent', () => {
+	beforeEach(() => {
+		req.body = newEvent;
+	});
 
-    beforeEach(() => {
-        req.body = newEvent;
-    });
+	it('Should have a createEvent function', () => {
+		expect(typeof EventController.createEvent).toBe('function');
+	});
 
-    it("Should have a createEvent function", () => {
-        expect(typeof EventController.createEvent).toBe("function");
-    });
+	it('Should call EventModel.create', () => {
+		EventController.createEvent(req, res, next);
+		expect(EventModel.create).toBeCalledWith(newEvent);
+	});
 
-    it("Should call EventModel.create", () => {  
-        EventController.createEvent(req, res, next);
-        expect(EventModel.create).toBeCalledWith(newEvent);
-    });
+	it('Should return 201 response code', async () => {
+		await EventController.createEvent(req, res, next);
+		expect(res.statusCode).toBe(201);
+		expect(res._isEndCalled()).toBeTruthy();
+	});
 
-    it("Should return 201 response code", async () => {
+	it('Should return json body in response', async () => {
+		EventModel.create.mockReturnValue(newEvent);
+		await EventController.createEvent(req, res, next);
+
+		expect(res._getJSONData()).toStrictEqual(newEvent);
+	});
+
+	it('Should handle errors', async () => {
+		const errorMessage = { message: 'Date property missing' };
+        const rejectedpromise = Promise.reject(errorMessage);
+
+        EventModel.create.mockReturnValue(rejectedpromise);
         await EventController.createEvent(req, res, next);
-        expect(res.statusCode).toBe(201);
-        expect(res._isEndCalled()).toBeTruthy();
-    });
 
-    it("Should return json body in response", async () => {
-        EventModel.create.mockReturnValue(newEvent);
-        await EventController.createEvent(req, res, next);
-
-        expect(res._getJSONData()).toStrictEqual(newEvent);
-    })
+        expect(next).toBeCalledWith(errorMessage);
+	});
 });
